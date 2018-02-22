45ac788886ad46b81805e4876b08102f8621ca34
Jose Francisco <chicojfp@gmail.com>
2018-02-22 09:54:11 -0300

    Adding missed file

---
+++ src/main/java/io/breezil/orquestra/exception/ExecutionException.java
@@ -0,0 +1,10 @@
+package io.breezil.orquestra.exception;
+
+public class ExecutionException extends OrquestraException {
+	private static final long serialVersionUID = -6659351527322496745L;
+
+	public ExecutionException(String errorMessage) {
+		super(errorMessage);
+	}
+
+}
