15d3687ba1f4fb84f5aedc4f4f7287f15c5e1e99
Jose Francisco Pereira <chicojfp@gmail.com>
2018-07-28 11:49:39 -0300

    Adding the partial selecting feature for SelectCommand.

--- src/main/java/io/breezil/orquestra/musico/commands/SelectCommand.java
+++ src/main/java/io/breezil/orquestra/musico/commands/SelectCommand.java
@@ -1,5 +1,7 @@
 package io.breezil.orquestra.musico.commands;
 
+import java.util.Objects;
+
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 
@@ -10,7 +12,7 @@ public class SelectCommand extends Command {
 	public SelectCommand() {
 		super();
 	}
-	
+
 	@Override
 	public boolean execute(ExecutionContext context) {
 		WebElementDefinition elInfo = context.getSearcher().findObjectDefinition(this.getItem());
@@ -18,12 +20,28 @@ public class SelectCommand extends Command {
 			WebElement el = context.getSearcher().findWebElement(this.updateXPathFilter(xpath));
 			if (el != null) {
 				Select select = new Select(el);
+				mapText2Select(select);
 				select.selectByVisibleText(this.value);
 				return true;
 			}
 		}
 
 		return false;
+	}
+
+	private String mapText2Select(Select select) {
+		if (!Objects.isNull(this.getPartial())) {
+			mapPartial2FullText(select);
+		}
+		return this.getValue();
+	}
+
+	private void mapPartial2FullText(Select select) {
+		select.getOptions().forEach(opt -> {
+			if (opt.getText().contains(this.getValue())) {
+				this.setValue(opt.getText());
+			}
+		});
 	}
 
 }
